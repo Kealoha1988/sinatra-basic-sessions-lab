78c506faf28ac900ee3eca3b5c918e866adbd0e2
Kealoha1988 <kealohaabe1988@gmail.com>
2021-01-05 10:03:02 -0500

    commit tree de4fa75cb0839aa8b667ee15c16abe6f33bc1009

--- app.rb
+++ app.rb
@@ -1,4 +1,33 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  # configure do
+  #   enable :sessions
+  #   set :session_secret, "sercret"
+  # end
+
+  # get "/" do
+  #   erb :index
+  # end
+
+
+  #   get '/hey' do
+  #     @session = session
+  #   end
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @sessions = session
+    item = params["item"]
+    @sessions[:item] = item
+  end
+
 end
